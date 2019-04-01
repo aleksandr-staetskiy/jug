770fd7aec1994c2a1234798de6fbac327ee4090f
Aleksandr Staetsky <myexploretime@gmail.com>
2019-04-01 18:28:08 +0300

    laptop and tablet version

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    Tipped.create('.report-tooltip--calendar', 'День 1');
+    Tipped.create('.report-tooltip--clock', '13:30');
+    Tipped.create('.report-tooltip--arrow', 'Зал 1');
+    Tipped.create('.report-tooltip--lang', 'EN');
+
+  });
+  
