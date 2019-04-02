9e11a4e5f00efa22fc5a9ab8bd514ba2a96188d4
Aleksandr Staetsky <myexploretime@gmail.com>
2019-04-02 16:24:08 +0300

    filter section adapted for mobile

--- js/script.js
+++ js/script.js
@@ -3,6 +3,13 @@ $(document).ready(function() {
     Tipped.create('.report-tooltip--clock', '13:30');
     Tipped.create('.report-tooltip--arrow', 'Зал 1');
     Tipped.create('.report-tooltip--lang', 'EN');
+  });
 
+  // burger menu
+  $('.burger, .overlay').click(function(){
+    $('.burger').toggleClass('clicked');
+    $('.overlay').toggleClass('show');
+    $('nav').toggleClass('show');
+    $('body').toggleClass('overflow');
   });
   
